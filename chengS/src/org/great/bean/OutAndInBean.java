43aee659263adc75740f2bc1a9bab0b788599c56
wc <wc@wc-PC>
2018-09-23 22:42:35 +0800

    出入库记录bean增加入库来源

--- chengS/src/org/great/bean/OutAndInBean.java
+++ chengS/src/org/great/bean/OutAndInBean.java
@@ -25,7 +25,7 @@ public class OutAndInBean {
 	private String destination;// 目的地
 	private String handleDate;// 操作时间
 	private int adminId;// 操作人员ID
-
+	private String drugSource;//入库来源
 	public OutAndInBean() {
 		super();
 	}
@@ -92,6 +92,14 @@ public class OutAndInBean {
 
 	public void setOutAndInId(int outAndInId) {
 		this.outAndInId = outAndInId;
+	}
+
+	public String getDrugSource() {
+		return drugSource;
+	}
+
+	public void setDrugSource(String drugSource) {
+		this.drugSource = drugSource;
 	}
 
 }
