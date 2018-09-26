06590ffa005609d2dd68bf9f564a7d0339b71d4b
jx1803 <43291171+jx1803@users.noreply.github.com>
2018-09-26 21:02:38 +0800

    Revert "完成药品采购审核，药品入库"

--- SPMS/src/org/great/biz/IStockWorkBiz.java
+++ SPMS/src/org/great/biz/IStockWorkBiz.java
@@ -14,12 +14,9 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.great.bean.BatchDetailBean;
 import org.great.bean.CondiBean;
 import org.great.bean.DrugApplyBean;
-import org.great.bean.InventoryBean;
 import org.great.bean.StoDrugBean;
-import org.springframework.web.servlet.ModelAndView;
 
 /** 
  * @ClassName: IStockWorkBiz 
@@ -29,11 +26,5 @@ import org.springframework.web.servlet.ModelAndView;
  */
 public interface IStockWorkBiz {
 	public String purchaseApply(HttpServletRequest request ,DrugApplyBean drugApply);
-	public List<StoDrugBean> stoDrugSearch(StoDrugBean stoDrugBean);//获取药品信息列表
-	public ModelAndView drugApplyAudit(CondiBean condiBean);//获取药品申请审核列表
-	public ModelAndView purchaseApplyLook(CondiBean condiBean);//获取药品采购申请记录列表
-	public String purchaseAuditFail(DrugApplyBean drugApplyBean);//药品采购申请审核不通过
-	public ModelAndView pdInstorage(CondiBean condiBean);//获取需购买的药品列表
-	public ModelAndView pdInstorageConfirm(DrugApplyBean drugApplyBean);//药品采购确认，修改购买状态
-	public String pdInstorageSuccess(BatchDetailBean batchDetailBean);//采购入库成功
+	public List<StoDrugBean> stoDrugSearch(StoDrugBean stoDrugBean);
 }
